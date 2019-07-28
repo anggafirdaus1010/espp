b445718726dc543697204f938e2d91ca1d34c1aa
angga <angga.firdaus1010@gmail.com>
2019-07-28 19:32:29 +0700

    Coba Kolaborasi

--- resources/views/site/root.blade.php
+++ resources/views/site/root.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{  __('str.app.name') }}
                 </div>
 
                 <div class="links">
